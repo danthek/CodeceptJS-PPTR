af24a7c42bd04ead3c9eabd2de3ba5a4da43efd0
danthek <abraham8507@gmail.com>
2022-08-30 14:13:20 -0500

    Test Scenarios with wait, grab, assertions, screenshots and elements visibility

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,24 +1,24 @@
-const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure')
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
-setHeadlessWhen(process.env.HEADLESS);
+setHeadlessWhen(process.env.HEADLESS)
 
 // enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
-setCommonPlugins();
+setCommonPlugins()
 
 /** @type {CodeceptJS.MainConfig} */
 exports.config = {
-  tests: './*_test.js',
-  output: './output',
-  helpers: {
-    Puppeteer: {
-      url: 'http://localhost',
-      show: true,
-      windowSize: '1200x900'
-    }
-  },
-  include: {
-    I: './steps_file.js'
-  },
-  name: 'CODECEPT-PPTR'
+	tests: './*_test.js',
+	output: './output',
+	helpers: {
+		Puppeteer: {
+			url: 'http://localhost',
+			show: false, // change it to false/true for headless function
+			windowSize: '1200x900',
+		},
+	},
+	include: {
+		I: './steps_file.js',
+	},
+	name: 'CODECEPT-PPTR',
 }
--- login_test.js
+++ login_test.js
@@ -1,5 +1,27 @@
-Feature('login');
-
-Scenario('test something', ({ I }) => {
-
-});
+const assert = require('assert')
+Feature('Sample Feature')
+// we are passing the object {I} which is a codeceptjs object and contains all the functions we need
+Scenario('First test', ({ I }) => {
+	I.amOnPage('https://www.example.com') //to open the browser
+	I.wait(1) // pass number on seconds, NOT miliseconds
+	I.saveScreenshot('test.png', true) //filename and full screen true/false(default)
+	I.see('Example') //if a text element is visible
+	I.dontSee('Berserk') // the opposite
+	I.seeElement('h1')
+	I.dontSeeElement('#noExistingDiv')
+})
+Scenario('Second test', ({ I }) => {
+	I.amOnPage('https://www.example.com') //to open the browser
+	I.wait(2) // pass number on seconds, NOT miliseconds
+})
+Scenario('Third test', async ({ I }) => {
+	I.amOnPage('https://www.example.com') //to open the browser
+	const text = await I.grabTextFrom('h1') // we ae grabbing/extracting a text value and storing it into a variable
+	console.log(text)
+	I.wait(3) // pass number on seconds, NOT miliseconds
+	assert.strictEqual(
+		text,
+		'Exacxczmple Domaisadn',
+		` This is the error message in case of failure, so the text didnt match with the "${text}" text`
+	) // value, string to match & error message in case of nedded.
+})
